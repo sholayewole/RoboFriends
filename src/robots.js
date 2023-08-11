ef537e05e5efaed168fdd329a370162bfa02faba
Sholayewole <sholayewole@gmail.com>
2023-08-11 10:35:31 +0100

    new robofriends

---
+++ src/robots.js
@@ -0,0 +1,56 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'John Laden',
+        username: 'Singles',
+        email: 'singulysingle@gmail.fin'
+    },
+    {
+        id: 2,
+        name: 'Phyna Odewu',
+        username: 'Phynity',
+        email: 'Phynityg@gmail.fin'
+    },
+    {
+        id: 3,
+        name: 'Capru Kemiade',
+        username: 'CapsK',
+        email: 'cappygurl@gmail.fin'
+    },
+    {
+        id: 4,
+        name: 'Ifemode Odunlayo',
+        username: 'Modela',
+        email: 'modelly@gmail.fin'
+    },
+    {
+        id: 5,
+        name: 'Sijuade Romola',
+        username: 'Romoks',
+        email: 'rommyrommy@gmail.fin'
+    },
+    {
+        id: 6,
+        name: 'Felicity Ayobade',
+        username: 'Citadel',
+        email: 'cicicity@gmail.fin'
+    },
+    {
+        id: 8,
+        name: 'Loberto Seas',
+        username: 'Obertos',
+        email: 'bertoseas@gmail.fin'
+    },
+    {
+        id: 9,
+        name: 'Fisherboy Fishers',
+        username: 'Fisherboy',
+        email: 'fishersfisher@gmail.fin'
+    },
+    {
+        id: 10,
+        name: 'Umeli Obaram',
+        username: 'Obali',
+        email: 'obaumeli@gmail.fin'
+    }
+]
